be907e9cddd1608576ce4be59610a1b7fe77b958
Siddhartha <siddhartha.ee.13@gmail.com>
2018-10-11 19:23:30 +0530

    Update src/pages/Login.js
    sdasdasd

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,18 +2,41 @@ import React from 'react';
 import { AppRegistry, StyleSheet, Alert, Text, Button, View } from 'react-native';
 
 export default class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true,
+                  dataSource:""}
+  }
+
   onScreenLoad() {
 
     setTimeout(function(){
       Alert.alert("Alert Shows After 5 Seconds of Delay.")
  
-    }, 10000);
+    }, 1000);
   }
+
+ componentDidMount(){
+     return fetch('https://creative-lizard.000webhostapp.com/getDetailse.php')
+    .then((response) => response.json())
+    .then((responseJson) => {
+       this.setState({
+          dataSource: JSON.stringify(responseJson),
+        }, function(){
+
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textStyle}>SAMPLE APP</Text>
+        <Text>{this.state.dataSource}</Text>
        <Button
           title="Register"
           onPress={() => this.props.navigation.navigate('Register')}
@@ -22,6 +45,8 @@ export default class Login extends React.Component {
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -38,4 +63,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  netTextStyle:{
+    color:'#000',
+    fontWeight:"bold",
+    fontSize: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
